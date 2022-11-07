52c55c7e1e0e2c3fdaaab3cbb15a0b190a673aba
Halogenetic <benjamin.akh@gmail.com>
2022-11-07 14:39:17 +0100

    final version corrected

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const weatherForm = document.getElementById('cityName')
 
 const weatherByDay = [[], [], [], [], []]
+const weatherByDay2 = [[], [], [], [], []]
+
 
 const container = document.querySelectorAll('.container')
 const container2 = document.querySelectorAll('.container2')
@@ -111,10 +113,10 @@ const getWeather2 = (cityName) =>{
 			let c = 0
 			data.list.forEach((weatherEntry) => {
 				if(i < 8){
-					weatherByDay[c].push(weatherEntry)
+					weatherByDay2[c].push(weatherEntry)
 				}else{
 					c++
-					weatherByDay[c].push(weatherEntry)
+					weatherByDay2[c].push(weatherEntry)
 					i = 0
 				}
 
@@ -124,69 +126,69 @@ const getWeather2 = (cityName) =>{
 			})
 
       container2[0].children[0].innerHTML=(cityName+" Today")
-      container2[0].children[1].innerHTML=(weatherByDay[0][0].main.temp+"°")
-      container2[0].children[2].children[0].src ="https://openweathermap.org/img/wn/" + weatherByDay[0][0].weather[0].icon + ".png";
-      container2[0].children[2].children[1].innerHTML=(weatherByDay[0][0].weather[0].description)
-      container2[0].children[3].innerHTML=("Humidity: "+ weatherByDay[0][0].main.humidity)
-      container2[0].children[4].innerHTML=("Wind: "+ weatherByDay[0][0].wind.speed)
-      
-      container2[1].children[0].innerHTML=(cityName+" Tomorrow")
-      container2[1].children[1].innerHTML=(weatherByDay[1][0].main.temp+"°")
-      container2[1].children[2].children[0].src ="https://openweathermap.org/img/wn/" + weatherByDay[1][0].weather[0].icon + ".png";
-      container2[1].children[2].children[1].innerHTML=(weatherByDay[1][0].weather[0].description)
-      container2[1].children[3].innerHTML=("Humidity: "+ weatherByDay[1][0].main.humidity)
-      container2[1].children[4].innerHTML=("Wind: "+ weatherByDay[1][0].wind.speed)
+container2[0].children[1].innerHTML=(weatherByDay2[0][0].main.temp+"°")
+container2[0].children[2].children[0].src ="https://openweathermap.org/img/wn/" + weatherByDay2[0][0].weather[0].icon + ".png";
+container2[0].children[2].children[1].innerHTML=(weatherByDay2[0][0].weather[0].description)
+container2[0].children[3].innerHTML=("Humidity: "+ weatherByDay2[0][0].main.humidity)
+container2[0].children[4].innerHTML=("Wind: "+ weatherByDay2[0][0].wind.speed)
 
-      container2[2].children[0].innerHTML=(cityName+" the day after")
-      container2[2].children[1].innerHTML=(weatherByDay[2][0].main.temp+"°")
-      container2[2].children[2].children[0].src ="https://openweathermap.org/img/wn/" + weatherByDay[2][0].weather[0].icon + ".png";
-      container2[2].children[2].children[1].innerHTML=(weatherByDay[2][0].weather[0].description)
-      container2[2].children[3].innerHTML=("Humidity: "+ weatherByDay[2][0].main.humidity)
-      container2[2].children[4].innerHTML=("Wind: "+ weatherByDay[2][0].wind.speed)
+container2[1].children[0].innerHTML=(cityName+" Tomorrow")
+container2[1].children[1].innerHTML=(weatherByDay2[1][0].main.temp+"°")
+container2[1].children[2].children[0].src ="https://openweathermap.org/img/wn/" + weatherByDay2[1][0].weather[0].icon + ".png";
+container2[1].children[2].children[1].innerHTML=(weatherByDay2[1][0].weather[0].description)
+container2[1].children[3].innerHTML=("Humidity: "+ weatherByDay2[1][0].main.humidity)
+container2[1].children[4].innerHTML=("Wind: "+ weatherByDay2[1][0].wind.speed)
 
-      container2[3].children[0].innerHTML=(cityName+" the day after after")
-      container2[3].children[1].innerHTML=(weatherByDay[3][0].main.temp+"°")
-      container2[3].children[2].children[0].src ="https://openweathermap.org/img/wn/" + weatherByDay[3][0].weather[0].icon + ".png";
-      container2[3].children[2].children[1].innerHTML=(weatherByDay[3][0].weather[0].description)
-      container2[3].children[3].innerHTML=("Humidity: "+ weatherByDay[3][0].main.humidity)
-      container2[3].children[4].innerHTML=("Wind: "+ weatherByDay[3][0].wind.speed)
+container2[2].children[0].innerHTML=(cityName+" the day after")
+container2[2].children[1].innerHTML=(weatherByDay2[2][0].main.temp+"°")
+container2[2].children[2].children[0].src ="https://openweathermap.org/img/wn/" + weatherByDay2[2][0].weather[0].icon + ".png";
+container2[2].children[2].children[1].innerHTML=(weatherByDay2[2][0].weather[0].description)
+container2[2].children[3].innerHTML=("Humidity: "+ weatherByDay2[2][0].main.humidity)
+container2[2].children[4].innerHTML=("Wind: "+ weatherByDay2[2][0].wind.speed)
 
-      container2[4].children[0].innerHTML=(cityName+" the day after after after")
-      container2[4].children[1].innerHTML=(weatherByDay[4][0].main.temp+"°")
-      container2[4].children[2].children[0].src ="https://openweathermap.org/img/wn/" + weatherByDay[4][0].weather[0].icon + ".png";
-      container2[4].children[2].children[1].innerHTML=(weatherByDay[4][0].weather[0].description)
-      container2[4].children[3].innerHTML=("Humidity: "+ weatherByDay[4][0].main.humidity)
-      container2[4].children[4].innerHTML=("Wind: "+ weatherByDay[4][0].wind.speed)
+container2[3].children[0].innerHTML=(cityName+" the day after after")
+container2[3].children[1].innerHTML=(weatherByDay2[3][0].main.temp+"°")
+container2[3].children[2].children[0].src ="https://openweathermap.org/img/wn/" + weatherByDay2[3][0].weather[0].icon + ".png";
+container2[3].children[2].children[1].innerHTML=(weatherByDay2[3][0].weather[0].description)
+container2[3].children[3].innerHTML=("Humidity: "+ weatherByDay2[3][0].main.humidity)
+container2[3].children[4].innerHTML=("Wind: "+ weatherByDay2[3][0].wind.speed)
 
-      document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + cityName + "')";
+container2[4].children[0].innerHTML=(cityName+" the day after after after")
+container2[4].children[1].innerHTML=(weatherByDay2[4][0].main.temp+"°")
+container2[4].children[2].children[0].src ="https://openweathermap.org/img/wn/" + weatherByDay2[4][0].weather[0].icon + ".png";
+container2[4].children[2].children[1].innerHTML=(weatherByDay2[4][0].weather[0].description)
+container2[4].children[3].innerHTML=("Humidity: "+ weatherByDay2[4][0].main.humidity)
+container2[4].children[4].innerHTML=("Wind: "+ weatherByDay2[4][0].wind.speed)
 
-      const remember = localStorage.setItem("choice", cityName)
+document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + cityName + "')";
 
-      var xyValues = [
-        {x:1, y:(weatherByDay[0][0].main.temp)},
-        {x:2, y:(weatherByDay[1][0].main.temp)},
-        {x:3, y:(weatherByDay[2][0].main.temp)},
-        {x:4, y:(weatherByDay[3][0].main.temp)},
-        {x:5, y:(weatherByDay[4][0].main.temp)},
-      ];
-      
-      new Chart("chart2", {
-        type: "scatter",
-        data: {
-          datasets: [{
-            pointRadius: 4,
-            pointBackgroundColor: "rgb(0,0,255)",
-            data: xyValues
-          }]
-        },
-        options: {
-          legend: {display: false},
-          scales: {
-            xAxes: [{ticks: {min: 1, max:5}}],
-            yAxes: [{ticks: {min: -5, max:30}}],
-          }
-        }
-      });
+const remember = localStorage.setItem("choice", cityName)
+
+var xyValues = [
+  {x:1, y:(weatherByDay2[0][0].main.temp)},
+  {x:2, y:(weatherByDay2[1][0].main.temp)},
+  {x:3, y:(weatherByDay2[2][0].main.temp)},
+  {x:4, y:(weatherByDay2[3][0].main.temp)},
+  {x:5, y:(weatherByDay2[4][0].main.temp)},
+];
+
+new Chart("chart2", {
+  type: "scatter",
+  data: {
+    datasets: [{
+      pointRadius: 4,
+      pointBackgroundColor: "rgb(0,0,255)",
+      data: xyValues
+    }]
+  },
+  options: {
+    legend: {display: false},
+    scales: {
+      xAxes: [{ticks: {min: 1, max:5}}],
+      yAxes: [{ticks: {min: -5, max:30}}],
+    }
+  }
+});
 
 		})
 
--- test.js
+++ test.js
@@ -1,34 +1,64 @@
-// container2[0].children[0].innerHTML=(cityName+" Today")
-//       container2[0].children[1].innerHTML=(weatherByDay[0][0].main.temp+"°")
-//       container2[0].children[2].children[0].src ="https://openweathermap.org/img/wn/" + weatherByDay[0][0].weather[0].icon + ".png";
-//       container2[0].children[2].children[1].innerHTML=(weatherByDay[0][0].weather[0].description)
-//       container2[0].children[3].innerHTML=("Humidity: "+ weatherByDay[0][0].main.humidity)
-//       container2[0].children[4].innerHTML=("Wind: "+ weatherByDay[0][0].wind.speed)
-      
-//       container2[1].children[0].innerHTML=(cityName+" Tomorrow")
-//       container2[1].children[1].innerHTML=(weatherByDay[1][0].main.temp+"°")
-//       container2[1].children[2].children[0].src ="https://openweathermap.org/img/wn/" + weatherByDay[1][0].weather[0].icon + ".png";
-//       container2[1].children[2].children[1].innerHTML=(weatherByDay[1][0].weather[0].description)
-//       container2[1].children[3].innerHTML=("Humidity: "+ weatherByDay[1][0].main.humidity)
-//       container2[1].children[4].innerHTML=("Wind: "+ weatherByDay[1][0].wind.speed)
+container2[0].children[0].innerHTML=(cityName+" Today")
+container2[0].children[1].innerHTML=(weatherByDay2[0][0].main.temp+"°")
+container2[0].children[2].children[0].src ="https://openweathermap.org/img/wn/" + weatherByDay2[0][0].weather[0].icon + ".png";
+container2[0].children[2].children[1].innerHTML=(weatherByDay2[0][0].weather[0].description)
+container2[0].children[3].innerHTML=("Humidity: "+ weatherByDay2[0][0].main.humidity)
+container2[0].children[4].innerHTML=("Wind: "+ weatherByDay2[0][0].wind.speed)
 
-//       container2[2].children[0].innerHTML=(cityName+" the day after")
-//       container2[2].children[1].innerHTML=(weatherByDay[2][0].main.temp+"°")
-//       container2[2].children[2].children[0].src ="https://openweathermap.org/img/wn/" + weatherByDay[2][0].weather[0].icon + ".png";
-//       container2[2].children[2].children[1].innerHTML=(weatherByDay[2][0].weather[0].description)
-//       container2[2].children[3].innerHTML=("Humidity: "+ weatherByDay[2][0].main.humidity)
-//       container2[2].children[4].innerHTML=("Wind: "+ weatherByDay[2][0].wind.speed)
+container2[1].children[0].innerHTML=(cityName+" Tomorrow")
+container2[1].children[1].innerHTML=(weatherByDay2[1][0].main.temp+"°")
+container2[1].children[2].children[0].src ="https://openweathermap.org/img/wn/" + weatherByDay2[1][0].weather[0].icon + ".png";
+container2[1].children[2].children[1].innerHTML=(weatherByDay2[1][0].weather[0].description)
+container2[1].children[3].innerHTML=("Humidity: "+ weatherByDay2[1][0].main.humidity)
+container2[1].children[4].innerHTML=("Wind: "+ weatherByDay2[1][0].wind.speed)
 
-//       container2[3].children[0].innerHTML=(cityName+" the day after after")
-//       container2[3].children[1].innerHTML=(weatherByDay[3][0].main.temp+"°")
-//       container2[3].children[2].children[0].src ="https://openweathermap.org/img/wn/" + weatherByDay[3][0].weather[0].icon + ".png";
-//       container2[3].children[2].children[1].innerHTML=(weatherByDay[3][0].weather[0].description)
-//       container2[3].children[3].innerHTML=("Humidity: "+ weatherByDay[3][0].main.humidity)
-//       container2[3].children[4].innerHTML=("Wind: "+ weatherByDay[3][0].wind.speed)
+container2[2].children[0].innerHTML=(cityName+" the day after")
+container2[2].children[1].innerHTML=(weatherByDay2[2][0].main.temp+"°")
+container2[2].children[2].children[0].src ="https://openweathermap.org/img/wn/" + weatherByDay2[2][0].weather[0].icon + ".png";
+container2[2].children[2].children[1].innerHTML=(weatherByDay2[2][0].weather[0].description)
+container2[2].children[3].innerHTML=("Humidity: "+ weatherByDay2[2][0].main.humidity)
+container2[2].children[4].innerHTML=("Wind: "+ weatherByDay2[2][0].wind.speed)
 
-//       container2[4].children[0].innerHTML=(cityName+" the day after after after")
-//       container2[4].children[1].innerHTML=(weatherByDay[4][0].main.temp+"°")
-//       container2[4].children[2].children[0].src ="https://openweathermap.org/img/wn/" + weatherByDay[4][0].weather[0].icon + ".png";
-//       container2[4].children[2].children[1].innerHTML=(weatherByDay[4][0].weather[0].description)
-//       container2[4].children[3].innerHTML=("Humidity: "+ weatherByDay[4][0].main.humidity)
-//       container2[4].children[4].innerHTML=("Wind: "+ weatherByDay[4][0].wind.speed)
+container2[3].children[0].innerHTML=(cityName+" the day after after")
+container2[3].children[1].innerHTML=(weatherByDay2[3][0].main.temp+"°")
+container2[3].children[2].children[0].src ="https://openweathermap.org/img/wn/" + weatherByDay2[3][0].weather[0].icon + ".png";
+container2[3].children[2].children[1].innerHTML=(weatherByDay2[3][0].weather[0].description)
+container2[3].children[3].innerHTML=("Humidity: "+ weatherByDay2[3][0].main.humidity)
+container2[3].children[4].innerHTML=("Wind: "+ weatherByDay2[3][0].wind.speed)
+
+container2[4].children[0].innerHTML=(cityName+" the day after after after")
+container2[4].children[1].innerHTML=(weatherByDay2[4][0].main.temp+"°")
+container2[4].children[2].children[0].src ="https://openweathermap.org/img/wn/" + weatherByDay2[4][0].weather[0].icon + ".png";
+container2[4].children[2].children[1].innerHTML=(weatherByDay2[4][0].weather[0].description)
+container2[4].children[3].innerHTML=("Humidity: "+ weatherByDay2[4][0].main.humidity)
+container2[4].children[4].innerHTML=("Wind: "+ weatherByDay2[4][0].wind.speed)
+
+document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + cityName + "')";
+
+const remember = localStorage.setItem("choice", cityName)
+
+var xyValues = [
+  {x:1, y:(weatherByDay2[0][0].main.temp)},
+  {x:2, y:(weatherByDay2[1][0].main.temp)},
+  {x:3, y:(weatherByDay2[2][0].main.temp)},
+  {x:4, y:(weatherByDay2[3][0].main.temp)},
+  {x:5, y:(weatherByDay2[4][0].main.temp)},
+];
+
+new Chart("chart2", {
+  type: "scatter",
+  data: {
+    datasets: [{
+      pointRadius: 4,
+      pointBackgroundColor: "rgb(0,0,255)",
+      data: xyValues
+    }]
+  },
+  options: {
+    legend: {display: false},
+    scales: {
+      xAxes: [{ticks: {min: 1, max:5}}],
+      yAxes: [{ticks: {min: -5, max:30}}],
+    }
+  }
+});
